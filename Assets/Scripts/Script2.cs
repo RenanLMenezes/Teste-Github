599ee095019e899ee5508a9e1f7cdeccb04d4c9d
RenanLMenezes <renanlmenezes2003@gmail.com>
2021-09-11 10:50:49 -0300

    changed script 2

--- Assets/Scripts/Script2.cs
+++ Assets/Scripts/Script2.cs
@@ -7,7 +7,7 @@ public class Script2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float vector6;
     }
 
     // Update is called once per frame
