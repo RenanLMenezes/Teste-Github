5a94ffac8e30f1a95e9c0c66c87c1797667460a5
RenanLMenezes <renanlmenezes2003@gmail.com>
2021-09-11 10:46:27 -0300

    changed script 1

--- Assets/Scripts/Scripts.cs
+++ Assets/Scripts/Scripts.cs
@@ -7,7 +7,7 @@ public class Scripts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var x = 0;
     }
 
     // Update is called once per frame
